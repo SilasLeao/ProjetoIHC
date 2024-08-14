9af91f32bdf6feba47f57a57b4387b7f9c58bb3d
SilasLeão <leaosilas@gmail.com>
2024-08-14 10:52:12 -0300

    .

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,9 +1,19 @@
 import "./Footer.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faFacebook,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 export default function Footer() {
   return (
     <footer>
-      <p>alow</p>
+      <div className="socials">
+        <FontAwesomeIcon className="dropdownIcon" icon={faInstagram} />
+        <FontAwesomeIcon className="dropdownIcon" icon={faFacebook} />
+        <FontAwesomeIcon className="dropdownIcon" icon={faWhatsapp} />
+      </div>
+      <p>MedLink© - Todos os Direitos Reservados</p>
     </footer>
   );
 }
